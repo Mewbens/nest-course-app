7f70d67041d82464c8b4a98e7b6d7c9d1639eda2
Benyapa Suntawareerit <benyapa.bs06@gmail.com>
2025-02-14 10:10:29 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('showInfo')
+  getInfo(): string {
+    return this.appService.getInfo();
+  }
+  @Get('/showmyjson')
+  getmyJson() {
+    return this.appService.getmyJson();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS';
+  }
+  getInfo(): string {
+    return 'Hello, I am Benyapa Suntawareerit , 20 years old.';
+  }
+  getmyJson() {
+    return {
+      name: 'Benyapa',
+      lastname: 'Suntawareerit',
+      age: 20,
+    };
+  }
+}
